8f6bda3360eba83c9a4a9e440ad212e78100d679
= <=>
2023-05-04 22:05:04 +0200

    App looks done

--- webservice/views.py
+++ webservice/views.py
@@ -1,5 +1,6 @@
 from os import getenv
 
+import requests
 from django.shortcuts import render
 
 from webservice.forms import SentimentForm
@@ -13,10 +14,18 @@ load_dotenv()
 def home_view(request):
     server_url = getenv('SERVER_URL')
     form = SentimentForm(request.POST or None)
+    smiley_emoji = "&#128577;"  # ""
+
     if form.is_valid():
-        form.save()
+        # response = requests.post(server_url).json()
+
+        is_positive = True # TODO: needs to be equal to a boolean constraint on the response
+
+        smiley_emoji = "&#128578;" if is_positive else "&#128577;"
     context = {
         'form': form,
-        'url': server_url
+        'server_url': server_url,
+        'smiley_emoji': smiley_emoji
+
     }
     return render(request, "webservice/index.html", context)
