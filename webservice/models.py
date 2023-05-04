c6962dc025eec0df4806f88c3ba8611c04b2c0ae
= <=>
2023-05-04 22:09:29 +0200

    Layout finished.

--- webservice/models.py
+++ webservice/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Review(models.Model):
-    review_text = models.CharField(max_length=500)
+    review_text = models.TextField(max_length=1000)
     no_of_stars = models.PositiveIntegerField()
 
     def __str__(self):
