93b08025d8d7c93e7f8050c74629148a42074cfd
Kostis Andriopoulos <konstantinosand@gmail.com>
2023-05-17 12:32:59 +0200

    Improve linting

--- app.py
+++ app.py
@@ -15,16 +15,17 @@ server_url = getenv('MODEL_SERVICE_URL', "http://0.0.0.0:8000")
 ALL_PREDICTIONS = 0
 CORRECT_PREDICTIONS = 0
 FALSE_PREDICTIONS = 0
-countHello = 0
-countValidate = 0
-countSubmit = 0
+COUNT_HELLO = 0
+COUNT_VALIDATE = 0
+COUNT_SUBMIT = 0
 
 
 class ReviewForm(FlaskForm):
-    """
-    Create text area field for review
-    """
-    review = TextAreaField('Review', validators=[validators.DataRequired()], render_kw={"rows": 5, "cols": 36})
+    """Create text area field for review."""
+
+    review = TextAreaField('Review',
+                           validators=[validators.DataRequired()],
+                           render_kw={"rows": 5, "cols": 36})
 
 
 class ValidationForm(FlaskForm):
@@ -40,8 +41,8 @@ class ValidationForm(FlaskForm):
 @app.route("/validate", methods=['POST'])
 def validate():
     """Process the feedback from the validation form in `index.html`."""
-    global CORRECT_PREDICTIONS, FALSE_PREDICTIONS, countValidate
-    countValidate += 1
+    global CORRECT_PREDICTIONS, FALSE_PREDICTIONS, COUNT_VALIDATE
+    COUNT_VALIDATE += 1
     validation_form = ValidationForm()
     if validation_form.validate_on_submit():
         prediction_is_correct = (validation_form.is_correct.data ==
@@ -51,7 +52,8 @@ def validate():
         else:
             FALSE_PREDICTIONS += 1
         requests.post(server_url + "/validate",
-                      {"validation": json.dumps(prediction_is_correct)})
+                      {"validation": json.dumps(prediction_is_correct)},
+                      timeout=1.5)
         # Show a thank you message and redirect the user to the home page
         return render_template("thanks.html")
     return redirect("/", 301)
@@ -60,14 +62,15 @@ def validate():
 @app.route("/submit", methods=['POST'])
 def submit():
     """Send the data from the text field to the server."""
-    global ALL_PREDICTIONS, countSubmit
-    countSubmit += 1
+    global ALL_PREDICTIONS, COUNT_SUBMIT
+    COUNT_SUBMIT += 1
 
     review_form = ReviewForm()
     if review_form.validate_on_submit():
         response: dict = requests.post(
             server_url + "/predict",
-            {"data": review_form.review.data}).json()
+            {"data": review_form.review.data},
+            timeout=1.5).json()
         ALL_PREDICTIONS += 1
         is_positive = response.get('sentiment', 0) == 1
         smiley_emoji = "&#128578;" if is_positive else "&#128577;"
@@ -83,8 +86,8 @@ def submit():
 @app.route("/", methods=['GET'])
 def hello():
     """Home page."""
-    global countHello
-    countHello += 1
+    global COUNT_HELLO
+    COUNT_HELLO += 1
     review_form = ReviewForm()
     return render_template("index.html",
                            review_form=review_form,
@@ -95,19 +98,19 @@ def hello():
 @app.route('/metrics', methods=['GET'])
 def metrics():
     """Send metrics for monitoring to Prometheus."""
-    global ALL_PREDICTIONS, CORRECT_PREDICTIONS, FALSE_PREDICTIONS, countHello, countSubmit, countValidate
+    global ALL_PREDICTIONS, CORRECT_PREDICTIONS, FALSE_PREDICTIONS
+    global COUNT_HELLO, COUNT_SUBMIT, COUNT_VALIDATE
 
     m = "# HELP predictions The number of predictions.\n"
     m += "# TYPE predictions counter\n"
     m += "predictions{{correct=\"None\"}} {}\n".format(ALL_PREDICTIONS)
     m += "predictions{{correct=\"True\"}} {}\n".format(CORRECT_PREDICTIONS)
     m += "predictions{{correct=\"False\"}} {}\n".format(FALSE_PREDICTIONS)
-    
+
     m += "# HELP num_requests The number of requests.\n"
     m += "# TYPE num_requests counter\n"
-    m += "num_requests{{page=\"index\"}} {}\n".format(countHello)
-    m += "num_requests{{page=\"validate\"}} {}\n".format(countValidate)
-    m += "num_requests{{page=\"submit\"}} {}\n".format(countSubmit)
-        
+    m += "num_requests{{page=\"index\"}} {}\n".format(COUNT_HELLO)
+    m += "num_requests{{page=\"validate\"}} {}\n".format(COUNT_VALIDATE)
+    m += "num_requests{{page=\"submit\"}} {}\n".format(COUNT_SUBMIT)
 
     return Response(m, mimetype="text/plain")
