77cddf48b4865a6d9a4920ddc5afe12a14719f91
Rohan Sobha <r.r.sobha@student.tudelft.nl>
2023-05-24 16:22:39 +0200

    Removed Prometheus metrics from app and moved to model-service.

--- app.py
+++ app.py
@@ -12,13 +12,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = urandom(32)
 server_url = getenv('MODEL_SERVICE_URL', "http://0.0.0.0:8000")
 
-ALL_PREDICTIONS = 0
-CORRECT_PREDICTIONS = 0
-FALSE_PREDICTIONS = 0
-COUNT_HELLO = 0
-COUNT_VALIDATE = 0
-COUNT_SUBMIT = 0
-
 
 class ReviewForm(FlaskForm):
     """Create text area field for review."""
@@ -41,16 +34,10 @@ class ValidationForm(FlaskForm):
 @app.route("/validate", methods=['POST'])
 def validate():
     """Process the feedback from the validation form in `index.html`."""
-    global CORRECT_PREDICTIONS, FALSE_PREDICTIONS, COUNT_VALIDATE # pylint: disable=W0603
-    COUNT_VALIDATE += 1
     validation_form = ValidationForm()
     if validation_form.validate_on_submit():
         prediction_is_correct = (validation_form.is_correct.data ==
                                  validation_form.thumbs_up)
-        if prediction_is_correct:
-            CORRECT_PREDICTIONS += 1
-        else:
-            FALSE_PREDICTIONS += 1
         requests.post(server_url + "/validate",
                       {"validation": json.dumps(prediction_is_correct)},
                       timeout=1.5)
@@ -62,16 +49,12 @@ def validate():
 @app.route("/submit", methods=['POST'])
 def submit():
     """Send the data from the text field to the server."""
-    global ALL_PREDICTIONS, COUNT_SUBMIT # pylint: disable=W0603
-    COUNT_SUBMIT += 1
-
     review_form = ReviewForm()
     if review_form.validate_on_submit():
         response: dict = requests.post(
             server_url + "/predict",
             {"data": review_form.review.data},
             timeout=1.5).json()
-        ALL_PREDICTIONS += 1
         is_positive = response.get('sentiment', 0) == 1
         smiley_emoji = "&#128578;" if is_positive else "&#128577;"
         validation_form = ValidationForm()
@@ -86,31 +69,8 @@ def submit():
 @app.route("/", methods=['GET'])
 def hello():
     """Home page."""
-    global COUNT_HELLO # pylint: disable=W0603
-    COUNT_HELLO += 1
     review_form = ReviewForm()
     return render_template("index.html",
                            review_form=review_form,
                            validation_form=None,
                            current_version=VersionUtil.get_version())
-
-
-@app.route('/metrics', methods=['GET'])
-def metrics():
-    """Send metrics for monitoring to Prometheus."""
-    global ALL_PREDICTIONS, CORRECT_PREDICTIONS, FALSE_PREDICTIONS # pylint: disable=W0602
-    global COUNT_HELLO, COUNT_SUBMIT, COUNT_VALIDATE # pylint: disable=W0602
-
-    m = "# HELP predictions The number of predictions.\n" # pylint: disable=C0103
-    m += "# TYPE predictions counter\n" # pylint: disable=C0103
-    m += "predictions{{correct=\"None\"}} {}\n".format(ALL_PREDICTIONS) # pylint: disable={C0103, C0209}
-    m += "predictions{{correct=\"True\"}} {}\n".format(CORRECT_PREDICTIONS) # pylint: disable={C0103, C0209}
-    m += "predictions{{correct=\"False\"}} {}\n".format(FALSE_PREDICTIONS) # pylint: disable={C0103, C0209}
-
-    m += "# HELP num_requests The number of requests.\n" # pylint: disable={C0103, C0209}
-    m += "# TYPE num_requests counter\n" # pylint: disable={C0103, C0209}
-    m += "num_requests{{page=\"index\"}} {}\n".format(COUNT_HELLO) # pylint: disable={C0103, C0209}
-    m += "num_requests{{page=\"validate\"}} {}\n".format(COUNT_VALIDATE) # pylint: disable={C0103, C0209}
-    m += "num_requests{{page=\"submit\"}} {}\n".format(COUNT_SUBMIT) # pylint: disable={C0103, C0209}
-
-    return Response(m, mimetype="text/plain")
