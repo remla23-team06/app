b50e75d88c30a79369c802b5c91cfe6d9852e483
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2023-06-24 02:01:39 +0200

    fix rating review submission

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-"""This module contains the flask server running the web-app."""
 import json
 from os import getenv, urandom
 import requests
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, make_response
 from flask_wtf import FlaskForm
 from wtforms import validators, TextAreaField, RadioField
 from remlaverlib.version_util import VersionUtil
@@ -10,7 +9,7 @@ from remlaverlib.version_util import VersionUtil
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = urandom(32)
-server_url = getenv('MODEL_SERVICE_URL', "http://0.0.0.0:8000")
+server_url = "http://localhost:8000"  # getenv('MODEL_SERVICE_URL', "http://0.0.0.0:8000")
 
 
 class ReviewForm(FlaskForm):
@@ -21,29 +20,24 @@ class ReviewForm(FlaskForm):
                            render_kw={"rows": 5, "cols": 36})
 
 
-class ValidationForm(FlaskForm):
-    """Create radio button field for validating the prediction."""
-
-    thumbs_up = "&#x1F44D;"
-    thumbs_down = "&#x1F44E;"
-    is_correct = RadioField('Correct prediction',
-                            validators=[validators.DataRequired()],
-                            choices=[thumbs_up, thumbs_down])
-
-
 @app.route("/validate", methods=['POST'])
 def validate():
     """Process the feedback from the validation form in `index.html`."""
-    validation_form = ValidationForm()
-    if validation_form.validate_on_submit():
-        prediction_is_correct = (validation_form.is_correct.data ==
-                                 validation_form.thumbs_up)
-        requests.post(server_url + "/validate",
-                      {"validation": json.dumps(prediction_is_correct), "sender": "with-emojis"},
-                      timeout=20)
+    review = ReviewForm().review
+    rating_value = request.form.get('rating')  # Updated to 'rating' instead of 'ratingValue'
+
+    response = requests.post(
+        server_url + "/validate",
+        {"validation": json.dumps({'rating': rating_value, 'review': review.data}), "sender": "with-emojis"},
+        timeout=20)
+
+    # Check the response status code
+    if response.status_code == 200:
         # Show a thank you message and redirect the user to the home page
-        return render_template("thanks.html")
-    return redirect("/", 301)
+        return make_response(render_template("thanks.html"))
+    else:
+        # Handle the error case
+        return make_response("Validation request failed.", 500)
 
 
 @app.route("/submit", methods=['POST'])
@@ -51,18 +45,28 @@ def submit():
     """Send the data from the text field to the server."""
     review_form = ReviewForm()
     if review_form.validate_on_submit():
-        response: dict = requests.post(
+
+        response = requests.post(
             server_url + "/predict",
             {"data": review_form.review.data, "sender": "with-emojis"},
             timeout=20).json()
+
         is_positive = response.get('sentiment', 0) == 1
         smiley_emoji = "&#128578;" if is_positive else "&#128577;"
-        validation_form = ValidationForm()
-        return render_template("index.html",
-                               review_form=review_form,
-                               smiley_emoji=smiley_emoji,
-                               current_version=VersionUtil.get_version(),
-                               validation_form=validation_form)
+
+        # Call the validate function and get the response
+        validation_response = validate()
+
+        if validation_response.status_code == 200:
+            # Show a thank you message and redirect the user to the home page
+            return render_template("index.html",
+                                   review_form=review_form,
+                                   smiley_emoji=smiley_emoji,
+                                   current_version=VersionUtil.get_version())
+        else:
+            # Handle the error case
+            return "Validation request failed.", 500
+
     return redirect("/", 301)
 
 
@@ -74,3 +78,7 @@ def hello():
                            review_form=review_form,
                            validation_form=None,
                            current_version=VersionUtil.get_version())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
